c9ee38a17731884ac7a44a9cdd9f534d2bace0ac
alexander-chepur <chepur@mail.ru>
2015-03-08 23:32:14 +0300

    plot4.R ready. Code made more tidy

--- plot2.R
+++ plot2.R
@@ -27,8 +27,8 @@ data$Date_Time = strptime(paste(data$Date, data$Time),"%d/%m/%Y %H:%M:%S")
 png("plot2.png", height = 480, width = 480)
 
 # Construct new plot
-plot(data$Date_Time, data$Global_active_power, pch=NA, xlab="", 
-     ylab="Global Active Power (kilowatts)")
+plot(data$Date_Time, data$Global_active_power, pch = NA, xlab = "", 
+     ylab = "Global Active Power (kilowatts)")
 lines(data$Date_Time, data$Global_active_power)
 
 # Close device
--- plot4.R
+++ plot4.R
@@ -1,5 +1,5 @@
 #######################
-## This script constructs Plot 3
+## This script constructs Plot 4
 ##
 
 # Read all the file
@@ -23,19 +23,32 @@ rm(data_all)
 # Create new column with date and time comverted to R format
 data$Date_Time = strptime(paste(data$Date, data$Time),"%d/%m/%Y %H:%M:%S")
 
-# Open PNG device to write to plot3.png
-png("plot3.png", height = 480, width = 480)
+# Open PNG device to write to plot4.png
+png("plot4.png", height = 480, width = 480)
 
-# Construct new plot
-plot(data$Date_Time, data$Sub_metering_1, pch = NA, xlab = "", 
-     ylab = "Energy sub metering")
+# Construct new plots on 2 by 2 canvas 
+par(mfrow=c(2,2))
+
+plot(data$Date_Time, data$Global_active_power, pch=NA, xlab="", 
+     ylab="Global Active Power")
+lines(data$Date_Time, data$Global_active_power)
+
+
+plot(data$Date_Time, data$Voltage, ylab="Voltage", xlab="datetime", pch=NA)
+lines(data$Date_Time, data$Voltage)
+
+
+plot(data$Date_Time, data$Sub_metering_1, pch=NA, xlab="", 
+     ylab="Energy sub metering")
 lines(data$Date_Time, data$Sub_metering_1)
-lines(data$Date_Time, data$Sub_metering_2, col = "red")
-lines(data$Date_Time, data$Sub_metering_3, col = "blue")
-legend("topright", 
-       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
-       lty = c(1, 1, 1), 
-       col = c("black", "red", "blue"))
+lines(data$Date_Time, data$Sub_metering_2, col="red")
+lines(data$Date_Time, data$Sub_metering_3, col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       lty = c(1,1,1), col = c("black", "red", "blue"), bty = "n")
+
+
+with(data, plot(Date_Time, Global_reactive_power, xlab = "datetime", pch = NA))
+with(data, lines(Date_Time, Global_reactive_power))
 
 # Close device
 dev.off()
